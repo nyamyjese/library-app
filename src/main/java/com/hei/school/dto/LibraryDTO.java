e145f65712d5563635ed610620eacffd89888827
nyamyjese <hei.jese.2@gmail.com>
2026-06-08 18:07:05 +0100

    feat(dto): add DTO for library

---
+++ src/main/java/com/hei/school/dto/LibraryDTO.java
@@ -0,0 +1,13 @@
+package com.hei.school.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LibraryDTO {
+    private long libraryId;
+    private String name ;
+    private String address;
+    private String phone;
+}

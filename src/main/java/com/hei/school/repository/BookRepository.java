650f09b94632031c96f177a7cf5067f521acec3b
nyamyjese <hei.jese.2@gmail.com>
2026-06-08 20:58:29 +0100

    feat(repository): add BookRepository

---
+++ src/main/java/com/hei/school/repository/BookRepository.java
@@ -0,0 +1,25 @@
+package com.hei.school.repository;
+
+import com.hei.school.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByLibrary_LibraryId(Long libraryId);
+
+    List<Book> findByPublicationYear(Integer year);
+
+    List<Book> findAllByOrderByTitleAsc();
+
+    List<Book> findAllByOrderByBasePriceAsc();
+}

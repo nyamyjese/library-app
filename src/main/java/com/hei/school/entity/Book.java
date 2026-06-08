72dff85ed9a9bc6ccd90f2d9681fb66c9f4df6de
nyamyjese <hei.jese.2@gmail.com>
2026-06-08 17:52:04 +0100

    feat(entity): add Book entity

---
+++ src/main/java/com/hei/school/entity/Book.java
@@ -0,0 +1,32 @@
+package com.hei.school.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.tomcat.jni.Library;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(unique = true)
+    private String isbn;
+    private Integer publicationYear;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "library_id" , nullable = false)
+    private Library library;
+}

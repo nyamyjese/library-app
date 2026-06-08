3ec312bb0729596294f7484c4c4d75776ad090d4
mahery razafimanantsoa <mahery@devme.lan>
2026-06-08 13:30:44 +0200

    feat(author): Add author class and Book

---
+++ src/main/java/com/hei/school/models/Book.java
@@ -0,0 +1,38 @@
+package com.hei.school.models;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    private UUID book_id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "isbn", length = 13)
+    private String isbn;
+
+    @Column(name = "date_publication", nullable = false)
+    private LocalDate publication = LocalDate.now();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "book_author", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private List<Author> authors;
+
+}

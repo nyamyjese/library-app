9ac0ce159dc24c20cf09406af8a7d38688db7b82
mekill404 <hei.mahery.55@gmail.com>
2026-06-11 09:47:07 +0200

    feat(entity/book): add bookAuthor to represent de liaison with Book and genre

---
+++ src/main/java/com/hei/school/entity/BookGenre.java
@@ -0,0 +1,27 @@
+package com.hei.school.entity;
+
+import java.util.UUID;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "book_genres", uniqueConstraints = @UniqueConstraint(columnNames = {"book_id", "genre_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookGenre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "book_id", nullable = false)
+    private UUID bookId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id", nullable = false)
+    private Genre genre;
+}

d43f41203fb7bed5255042b03aec9767c8f2e418
Fiderana-17 <hei.fiderana@gmail.com>
2026-06-17 15:05:45 +0300

    refactor: replace manual constructors with Lombok in model/Arrival and model/BookCopy

--- src/main/java/com/hei/school/model/Arrival.java
+++ src/main/java/com/hei/school/model/Arrival.java
@@ -1,34 +1,22 @@
 package com.hei.school.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Arrival {
 
-  public Integer arrivalId;
-  public Integer bookId;
-  public Integer libraryId;
-  public BookFormat format;
-  public Integer quantity;
-  public LocalDate arrivalDate;
-  public BigDecimal unitCost;
-
-  public Arrival() {}
-
-  public Arrival(
-      Integer arrivalId,
-      Integer bookId,
-      Integer libraryId,
-      BookFormat format,
-      Integer quantity,
-      LocalDate arrivalDate,
-      BigDecimal unitCost) {
-    this.arrivalId = arrivalId;
-    this.bookId = bookId;
-    this.libraryId = libraryId;
-    this.format = format;
-    this.quantity = quantity;
-    this.arrivalDate = arrivalDate;
-    this.unitCost = unitCost;
-  }
+  private UUID arrivalId;
+  private UUID bookId;
+  private UUID libraryId;
+  private BookFormat format;
+  private Integer quantity;
+  private Instant arrivalDate;
+  private BigDecimal unitCost;
 }
--- src/main/java/com/hei/school/model/BookCopy.java
+++ src/main/java/com/hei/school/model/BookCopy.java
@@ -1,34 +1,23 @@
 package com.hei.school.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.Instant;
+import java.util.UUID;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookCopy {
 
-  public Integer copyId;
-  public Integer bookId;
-  public Integer libraryId;
-  public BookFormat format;
-  public BigDecimal copyPrice;
-  public CopyStatus status;
-  public LocalDate acquisitionDate;
-
-  public BookCopy() {}
-
-  public BookCopy(
-      Integer copyId,
-      Integer bookId,
-      Integer libraryId,
-      BookFormat format,
-      BigDecimal copyPrice,
-      CopyStatus status,
-      LocalDate acquisitionDate) {
-    this.copyId = copyId;
-    this.bookId = bookId;
-    this.libraryId = libraryId;
-    this.format = format;
-    this.copyPrice = copyPrice;
-    this.status = status;
-    this.acquisitionDate = acquisitionDate;
-  }
+  private UUID copyId;
+  private UUID bookId;
+  private UUID libraryId;
+  private BookFormat format;
+  private BigDecimal copyPrice;
+  private CopyStatus status;
+  private Instant acquisitionDate;
 }

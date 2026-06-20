7dcf369fea013d025c30e05002f63caa3ae494c0
nyamyjese <hei.jese.2@gmail.com>
2026-06-20 22:38:58 +0100

    chore: format

--- src/main/java/com/hei/school/exception/NotFoundException.java
+++ src/main/java/com/hei/school/exception/NotFoundException.java
@@ -1,7 +1,7 @@
 package com.hei.school.exception;
 
-public class NotFoundException extends RuntimeException{
-    public  NotFoundException(String message){
-        super(message);
-    }
+public class NotFoundException extends RuntimeException {
+  public NotFoundException(String message) {
+    super(message);
+  }
 }
